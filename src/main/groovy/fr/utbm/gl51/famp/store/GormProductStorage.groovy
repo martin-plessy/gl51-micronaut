b50b899acb2c4d7cb3589bc8c519b6844c588f3f
Martin <martin.plessy@utbm.fr>
2019-05-10 12:07:07 +0200

    Added GORM-based product storage template

---
+++ src/main/groovy/fr/utbm/gl51/famp/store/GormProductStorage.groovy
@@ -0,0 +1,25 @@
+package fr.utbm.gl51.famp.store
+
+class GormProductStorage implements ProductStorage {
+	@Override
+	void save(Product product) {
+	}
+
+	@Override
+	Product getById(String id) {
+		null
+	}
+
+	@Override
+	List<Product> all() {
+		null
+	}
+
+	@Override
+	void update(String id, Product p) {
+	}
+
+	@Override
+	void delete(String id) {
+	}
+}
