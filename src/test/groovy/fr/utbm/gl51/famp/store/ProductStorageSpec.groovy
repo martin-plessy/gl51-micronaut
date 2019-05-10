6ad35b2ce2ea82f6d7af90581afd7d94cf64d28d
Martin <martin.plessy@utbm.fr>
2019-05-10 11:24:47 +0200

    Did some refactoring as well

--- src/test/groovy/fr/utbm/gl51/famp/store/ProductStorageSpec.groovy
+++ src/test/groovy/fr/utbm/gl51/famp/store/ProductStorageSpec.groovy
@@ -3,58 +3,58 @@ package fr.utbm.gl51.famp.store
 import spock.lang.Specification
 
 class InMemoryProductStorageSpec extends Specification {
-	ProductStorage products = new InMemoryProductStorage()
-	ProductStorage filledProducts
-	def someProducts = [
+	ProductStorage emptyStorage = new InMemoryProductStorage()
+	ProductStorage filledStorage = new InMemoryProductStorage()
+
+	def sampleProducts = [
 		new Product(name: "One"),
 		new Product(name: "Two"),
 		new Product(name: "Three")
 	]
 
 	def setup() {
-		filledProducts = new InMemoryProductStorage()
-		someProducts.each {  filledProducts.save(it)}
+		sampleProducts.each { filledStorage.save(it) }
 	}
 
 	def "Empty storage | all => empty list"() {
 		expect:
-		products.all() == []
+		emptyStorage.all() == []
 	}
 
 
 	def "Empty storage | save => product retrievable"() {
 		setup:
 		def newProduct = new Product()
-		products.save(newProduct)
+		emptyStorage.save(newProduct)
 
 		expect:
-		products.all() == [ newProduct ]
+		emptyStorage.all() == [ newProduct ]
 	}
 
 	def "Empty storage | save => generates id"() {
 		setup:
 		def newProduct = new Product()
-		products.save(newProduct)
+		emptyStorage.save(newProduct)
 
 		expect:
-		products.all().first().id != ""
+		emptyStorage.all().first().id != ""
 		newProduct.id != ""
 	}
 
 	def "Empty storage | save * N => N product retrievable"() {
 		setup:
 		def N = 8
-		for (i in 1 .. N) products.save(new Product())
+		for (i in 1 .. N) emptyStorage.save(new Product())
 
 		expect:
-		products.all().size() == N
+		emptyStorage.all().size() == N
 	}
 
 	def "Empty storage | save => product retrievable by id"() {
 		setup:
 		def newProduct = new Product()
-		products.save(newProduct)
-		def retrievedProduct = products.getById(newProduct.id)
+		emptyStorage.save(newProduct)
+		def retrievedProduct = emptyStorage.getById(newProduct.id)
 
 		expect:
 		retrievedProduct == newProduct
@@ -62,7 +62,7 @@ class InMemoryProductStorageSpec extends Specification {
 
 	def "Filled storage | getById [not exists] => exception"() {
 		when:
-		def _ = filledProducts.getById("AAAAAC")
+		def _ = filledStorage.getById("AAAAAC")
 
 		then:
 		thrown NoSuchElementException
@@ -70,32 +70,32 @@ class InMemoryProductStorageSpec extends Specification {
 
 	def "Empty storage | delete => empty storage"() {
 		setup:
-		products.delete("AAAAAB")
+		emptyStorage.delete("AAAAAB")
 
 		expect:
-		products.all() == []
+		emptyStorage.all() == []
 	}
 
 	def "Filled storage | delete => product removed"() {
 		setup:
-		filledProducts.delete(someProducts[1].id)
+		filledStorage.delete(sampleProducts[1].id)
 
 		expect:
-		filledProducts.all() == [ someProducts[0], someProducts[2] ]
+		filledStorage.all() == [ sampleProducts[0], sampleProducts[2] ]
 	}
 
 	def "Filled storage | delete * 2 => idempotent delete"() {
 		setup:
-		filledProducts.delete(someProducts[1].id)
-		filledProducts.delete(someProducts[1].id)
+		filledStorage.delete(sampleProducts[1].id)
+		filledStorage.delete(sampleProducts[1].id)
 
 		expect:
-		filledProducts.all() == [ someProducts[0], someProducts[2] ]
+		filledStorage.all() == [ sampleProducts[0], sampleProducts[2] ]
 	}
 
 	def "Empty storage | update [not exists] => exception"() {
 		when:
-		products.update("AAAAAD", new Product(name: "Updated"))
+		emptyStorage.update("AAAAAD", new Product(name: "Updated"))
 
 		then:
 		thrown NoSuchElementException
@@ -103,13 +103,13 @@ class InMemoryProductStorageSpec extends Specification {
 
 	def "update storage "() {
 		setup:
-		filledProducts.update(someProducts[1].id, new Product(name: "Four"))
+		filledStorage.update(sampleProducts[1].id, new Product(name: "Four"))
 
 		expect:
-		filledProducts.all() == [
-			someProducts[0],
-			new Product(id: someProducts[1].id, name: "Four"),
-			someProducts[2]
+		filledStorage.all() == [
+			sampleProducts[0],
+			new Product(id: sampleProducts[1].id, name: "Four"),
+			sampleProducts[2]
 		]
 	}
 }
